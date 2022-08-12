d6435caafe16a27f2d1cff18080344f809fc3a89
tomasmansur <61087373+tomasmansur@users.noreply.github.com>
2022-08-12 16:13:45 -0300

    Update openwrt-snapshot-for-archer-c7-v5.sh

--- OpenWrt/imager-builder/openwrt-snapshot-for-archer-c7-v5.sh
+++ OpenWrt/imager-builder/openwrt-snapshot-for-archer-c7-v5.sh
@@ -5,7 +5,7 @@
 if [[ $EUID -eq 0 ]]; then
   apt update -qq
   apt install -y build-essential libncurses5-dev libncursesw5-dev \
-  zlib1g-dev gawk git gettext libssl-dev xsltproc rsync wget unzip python3
+  zlib1g-dev gawk git gettext libssl-dev xsltproc rsync wget unzip python
 
   mkdir --parents /opt/openwrt-snapshots
 fi
