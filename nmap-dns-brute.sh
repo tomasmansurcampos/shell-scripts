e1866fde1733e3a3e552eee60f12c34d91f2ba79
tomasmansur <61087373+tomasmansur@users.noreply.github.com>
2022-08-13 00:58:11 -0300

    Update nmap-dns-brute.sh

--- nmap-dns-brute.sh
+++ nmap-dns-brute.sh
@@ -3,8 +3,8 @@
 dns_brute_()
 {
   ## https://nmap.org/nsedoc/scripts/dns-brute.html
-  SCRIPT_ARGS="dns-brute.domain=$1,dns-brute.threads=$2" ## ,newtargets -sS -p 80 ????
-  nmap --script dns-brute --script-args $SCRIPT_ARGS
+  SCRIPT_ARGS_="dns-brute.domain=$1,dns-brute.threads=$2" ## ,newtargets -sS -p 80 ????
+  nmap --script dns-brute --script-args $SCRIPT_ARGS_
 }
 
 dns_brute_ $1 $2
