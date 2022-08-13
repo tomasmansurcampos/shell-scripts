74edb4d0dadd2f026994b1158808dfc430b34207
tomasmansur <61087373+tomasmansur@users.noreply.github.com>
2022-08-13 01:57:47 -0300

    Update tftp-server.sh

--- tftp-server.sh
+++ tftp-server.sh
@@ -11,13 +11,16 @@ echo '# /etc/default/tftpd-hpa
 TFTP_USERNAME="tftp"
 TFTP_DIRECTORY="/srv/tftp"
 TFTP_ADDRESS="192.168.0.66:69"
-TFTP_OPTIONS="--secure"' > $CONFIG_FILE_
+TFTP_OPTIONS="-4 --secure -vvv"' > $CONFIG_FILE_
 
 mkdir --parents /srv/tftp
 
 if id "user" &>/dev/null; then
 	chown -R tftp:tftp /srv/tftp
 fi
+
+mv $1 /srv/tftp/ArcherC7v5_tp_recovery.bin
+chown -R tftp:tftp /srv/tftp
 
 systemctl enable tftpd-hpa
 systemctl restart tftpd-hpa
