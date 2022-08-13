0f821054f562a2a9bca52e2ae7186fa97e1ab7f5
tomasmansur <61087373+tomasmansur@users.noreply.github.com>
2022-08-13 00:55:28 -0300

    Add files via upload

---
+++ musl-cc.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+musl_cc_()
+{
+	BASE_FOLDER_="~/musl-cc"
+	mkdir --parents $BASE_FOLDER_
+	curl -s musl.cc | grep mips | tee links
+	cd $BASE_FOLDER_
+	for $l_ in links:
+	do
+		if [[ $(wget -q --spider $l_) -eq "0" ]]; then
+			wget --https-only $l_
+	done
+	rm -rf links
+	cd ~
+}
+
+musl_cc_
