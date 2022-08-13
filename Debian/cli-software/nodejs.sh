393b0e5710ba68a97cbd334075b23e62a6d577bd
tomasmansur <61087373+tomasmansur@users.noreply.github.com>
2022-08-13 01:09:17 -0300

    Update nodejs.sh

--- Debian/cli-software/nodejs.sh
+++ Debian/cli-software/nodejs.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-nodejs_18_dot_x_()
+nodejs_()
 {
 	## https://nodejs.org/en/about/releases/
 	## https://github.com/nodesource/distributions/blob/master/README.md
-	curl -fsSL https://deb.nodesource.com/setup_18.x | bash -
+	VERSION_=18
+	curl -fsSL https://deb.nodesource.com/setup_$VERSION_.x | bash -
 	apt update
 	apt install -y nodejs
 }
 
-nodejs_18_dot_x_
+nodejs_
