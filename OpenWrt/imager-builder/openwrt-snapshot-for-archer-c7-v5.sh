746ed294783efcfe0be5504111c7b6c402e4b92a
tomasmansur <61087373+tomasmansur@users.noreply.github.com>
2022-08-13 00:02:59 -0300

    Update openwrt-snapshot-for-archer-c7-v5.sh

--- OpenWrt/imager-builder/openwrt-snapshot-for-archer-c7-v5.sh
+++ OpenWrt/imager-builder/openwrt-snapshot-for-archer-c7-v5.sh
@@ -3,14 +3,14 @@
 ## https://openwrt.org/docs/guide-user/additional-software/imagebuilder#prerequisites
 
 if [[ $EUID -eq 0 ]]; then
-  apt update -qq
+  apt update
   apt install -y build-essential libncurses5-dev libncursesw5-dev \
-  zlib1g-dev gawk git gettext libssl-dev xsltproc rsync wget unzip python
-
-  mkdir --parents /opt/openwrt-snapshots
+  zlib1g-dev gawk git gettext libssl-dev xsltproc rsync wget unzip python3
+  update-alternatives --install /usr/bin/python python /usr/bin/python3 1
 fi
 
-rm -rf /opt/openwrt-snapshots/archer-c7-v5
+mkdir --parents ~/openwrt-snapshots
+rm -rf ~/openwrt-snapshots/archer-c7-v5
 
 if [[ $(wget -q --spider https://downloads.openwrt.org/snapshots/targets/ath79/generic/openwrt-imagebuilder-ath79-generic.Linux-x86_64.tar.xz) -eq "0" ]]; then
     wget --https-only --inet4-only https://downloads.openwrt.org/snapshots/targets/ath79/generic/openwrt-imagebuilder-ath79-generic.Linux-x86_64.tar.xz
@@ -19,7 +19,7 @@ if [[ $(wget -q --spider https://downloads.openwrt.org/snapshots/targets/ath79/g
     cd openwrt-imagebuilder-ath79-generic.Linux-x86_64/
     source ../../packages.sh
     PROFILE_="tplink_archer-c7-v5"
-    BIN_DIR_="/opt/openwrt-snapshots/archer-c7-v5"
+    BIN_DIR_="~/openwrt-snapshots/archer-c7-v5"
     rm -rf $BIN_DIR_
     make -j $(expr $(nproc) - 1) image PROFILE=$PROFILE_ PACKAGES="$PACKAGES_BASIC_" BIN_DIR=$BIN_DIR_ 
     make clean
