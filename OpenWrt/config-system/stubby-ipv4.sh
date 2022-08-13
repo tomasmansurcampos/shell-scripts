c7854c59bf8174ded63bb0b8e6376f5eda575f2e
tomasmansur <61087373+tomasmansur@users.noreply.github.com>
2022-08-13 00:30:35 -0300

    Update stubby-ipv4.sh

--- OpenWrt/config-system/stubby-ipv4.sh
+++ OpenWrt/config-system/stubby-ipv4.sh
@@ -11,12 +11,12 @@ uci set stubby.global.tls_min_version="1.3"
 uci set stubby.global.tls_max_version="1.3"
 
 while uci -q delete stubby.@resolver[0]; do :; done
-## getdnsapi.net
+## https://dnsprivacy.org/test_servers/
 uci add stubby resolver
 uci set stubby.@resolver[-1].address="185.49.141.37"
 uci set stubby.@resolver[-1].tls_auth_name="getdnsapi.net"
 uci add_list stubby.@resolver[-1].spki="sha256/foxZRnIh9gZpWnl+zEiKa0EJ2rdCGroMWm02gaxSc9Q="
-## dns.sb
+## https://dns.sb/dot/
 uci add stubby resolver
 uci set stubby.@resolver[-1].address="45.11.45.11"
 uci set stubby.@resolver[-1].tls_auth_name="dot.sb"
