c78af2015abcea9e352bdb40fb0eee80f68228de
tomasmansur <61087373+tomasmansur@users.noreply.github.com>
2022-09-08 19:57:27 -0300

    Create gnu-c-reference.sh

---
+++ gnu-c-reference.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+gnu_c_reference_()
+{
+  apt update
+  apt install -y git texinfo texlive
+  cd c-intro-and-ref
+  mkdir c-manual
+  ln Makefile c.texi cpp.texi fp.texi fdl.texi c-manual
+  tar czf c-manual.tgz c-manual
+  cd c-manual
+  texi2pdf c.texi
+  cp c.pdf $HOME
+  cd ../..
+  rm -rf c-intro-and-ref
+  cd $HOME
+}
+gnu_c_reference_
