0a63d25300a307d376ea199431af57e024726ded
tomasmansur <61087373+tomasmansur@users.noreply.github.com>
2022-09-08 20:06:06 -0300

    Update gnu-c-reference.sh

--- gnu-c-reference.sh
+++ gnu-c-reference.sh
@@ -3,6 +3,7 @@ gnu_c_reference_()
 {
   apt update
   apt install -y git texinfo texlive
+  git clone https://git.savannah.gnu.org/git/c-intro-and-ref.git
   cd c-intro-and-ref
   mkdir c-manual
   ln Makefile c.texi cpp.texi fp.texi fdl.texi c-manual
