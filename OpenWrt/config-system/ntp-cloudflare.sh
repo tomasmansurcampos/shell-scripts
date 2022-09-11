8ca7ab590a14886a1113c4ef7bf765eb10513bab
tomasmansur <61087373+tomasmansur@users.noreply.github.com>
2022-09-11 15:01:27 -0300

    Update and rename ntp-google.sh to ntp-cloudflare.sh

---
+++ OpenWrt/config-system/ntp-cloudflare.sh
@@ -0,0 +1,12 @@
+#!/bin/ash
+
+/etc/init.d/sysntpd stop
+
+#uci set system.ntp=timeserver
+uci set system.ntp.enabled="1"
+uci set system.ntp.enable_server="1"
+uci -q delete system.ntp.server
+uci add_list system.ntp.server="time.cloudflare.com"
+
+uci commit system
+/etc/init.d/sysntpd restart
