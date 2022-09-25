64854244595a07d06e791eb05e31d4597b159473
tomasmansur <61087373+tomasmansur@users.noreply.github.com>
2022-09-25 00:48:50 -0300

    Update musl-compiler.sh

--- Debian/cli-software/musl-compiler.sh
+++ Debian/cli-software/musl-compiler.sh
@@ -10,19 +10,20 @@ musl_compiler_()
     mkdir --parents $BASE_FOLDER_ >/dev/null 2>&1
     rm -rf $MUSL_FOLDER_
     rm -rf /lib/ld-musl*
+    cd /usr/local/src/musl
+    make uninstall
     mkdir --parents $MUSL_FOLDER_ >/dev/null 2>&1
 
 	apt update
     apt install -y gcc make git
-    git clone git://git.musl-libc.org/musl
-    cd musl/
+    git clone git://git.musl-libc.org/musl /usr/local/src/musl
+    cd /usr/local/src/musl
     ./configure --bindir=$MUSL_FOLDER_/bin --libdir=$MUSL_FOLDER_/lib --includedir=$MUSL_FOLDER_/include --syslibdir=/lib
     make -j $(expr $(nproc) - 1)
     make install
     make clean
-    cd ..
-    rm -rf musl/
     ln -sf /opt/software/musl/bin/musl-gcc /usr/bin/musl-gcc
+    cd $HOME
 }
 
 musl_compiler_
