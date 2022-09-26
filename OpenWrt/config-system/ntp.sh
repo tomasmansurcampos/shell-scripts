393ba47a9697cb35a0dfcca441888e6b0df7fedd
tomasmansur <61087373+tomasmansur@users.noreply.github.com>
2022-09-25 22:39:20 -0300

    Update and rename ntp-cloudflare.sh to ntp.sh

--- OpenWrt/config-system/ntp.sh
+++ OpenWrt/config-system/ntp.sh
@@ -6,7 +6,7 @@
 uci set system.ntp.enabled="1"
 uci set system.ntp.enable_server="1"
 uci -q delete system.ntp.server
-uci add_list system.ntp.server="time.cloudflare.com"
+uci add_list system.ntp.server="pool.ntp.org"
 
 uci commit system
 /etc/init.d/sysntpd restart
