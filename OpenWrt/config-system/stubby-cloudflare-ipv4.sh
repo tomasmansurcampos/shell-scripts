99c6209275aa03f1ed006fca07199ec679198e90
tomasmansur <61087373+tomasmansur@users.noreply.github.com>
2022-09-25 22:37:50 -0300

    Update stubby-cloudflare-ipv4.sh

--- OpenWrt/config-system/stubby-cloudflare-ipv4.sh
+++ OpenWrt/config-system/stubby-cloudflare-ipv4.sh
@@ -13,6 +13,7 @@ stubby_()
   uci set stubby.global.tls_connection_retries="1"
   uci set stubby.global.tls_min_version="1.3"
   uci set stubby.global.tls_max_version="1.3"
+  uci set stubby.global.idle_timeout='9000' ## (NOTE: recommend reducing idle_timeout to 9000 if using Cloudflare)
 
   while uci -q delete stubby.@resolver[0]; do :; done
   uci add stubby resolver
