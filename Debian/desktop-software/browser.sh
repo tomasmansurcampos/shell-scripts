6ff4ca917d3ee516d128b450138664d42790a12e
tomasmansur <61087373+tomasmansur@users.noreply.github.com>
2022-10-01 12:17:03 -0300

    Update and rename firefox.sh to browser.sh

--- Debian/desktop-software/browser.sh
+++ Debian/desktop-software/browser.sh
@@ -2,6 +2,7 @@
 ## https://ftp.mozilla.org/pub/firefox/releases/latest/README.txt
 firefox_stable_()
 {
+	/usr/bin/apt --purge autoremove -y firefox*
 	rm -rf /home/*/.cache/mozill*
 	rm -rf /home/*/.mozill*
 	rm -rf /usr/lib/mozilla/
@@ -12,18 +13,17 @@ firefox_stable_()
 	## differs from the Official Firefox Website https://www.mozilla.org/en-US/firefox/all/#product-desktop-release
 	## from product=firefox-latest to product=firefox-latest-ssl...
 	## i follow the link that points https://www.mozilla.org/ not https://ftp.mozilla.org/pub/firefox/releases/latest/README.txt
-	BASE_FOLDER_=/opt/software
+	BASE_FOLDER_=/usr/local/share
+	apt update
 	apt install -y libdbus-glib-1-2
 	wget --https-only -O firefox.tar.bz2 "https://download.mozilla.org/?product=firefox-latest-ssl&os=linux64&lang=en-US"
 	tar xf firefox.tar.bz2
 	rm -rf firefox.tar.bz2
 	mv firefox $BASE_FOLDER_
-	## creation for void user:
-	chown user:user -R /opt/software
 	##
 	rm -rf /usr/local/bin/firefox
 	rm -rf /usr/bin/firefox
-	ln -s $BASE_FOLDER_/firefox/firefox /usr/bin/firefox
+	ln -sf $BASE_FOLDER_/firefox/firefox /usr/bin/firefox
 	echo "[Desktop Entry]
 Version=1.0
 Name=Firefox Stable
@@ -31,10 +31,21 @@ Comment=Web Browser
 Exec=firefox %u
 Terminal=false
 Type=Application
-Icon=/opt/software/firefox/browser/chrome/icons/default/default128.png
+Icon=/usr/local/share/firefox/browser/chrome/icons/default/default128.png
 Categories=Network;WebBrowser;
 MimeType=text/html;text/xml;application/xhtml+xml;application/xml;application/vnd.mozilla.xul+xml;application/rss+xml;application/rdf+xml;image/gif;image/jpeg;image/png;x-scheme-handler/http;x-scheme-handler/https;
 StartupNotify=true" > /usr/share/applications/firefox.desktop
+	echo "[Desktop Entry]
+Version=1.0
+Name=Firefox Stable (Private mode)
+Comment=Web Browser
+Exec=firefox --private-window %u
+Terminal=false
+Type=Application
+Icon=/usr/local/share/firefox/browser/chrome/icons/default/default128.png
+Categories=Network;WebBrowser;
+MimeType=text/html;text/xml;application/xhtml+xml;application/xml;application/vnd.mozilla.xul+xml;application/rss+xml;application/rdf+xml;image/gif;image/jpeg;image/png;x-scheme-handler/http;x-scheme-handler/https;
+StartupNotify=true" > /usr/share/applications/firefox-private.desktop
 	#update-alternatives --install /usr/bin/gnome-www-browser gnome-www-browser /opt/firefox/firefox 200
 	#update-alternatives --set  gnome-www-browser /opt/firefox/firefox
 	#update-alternatives --install /usr/bin/x-www-browser x-www-browser /opt/firefox/firefox 200
