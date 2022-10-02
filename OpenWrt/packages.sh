454f9d30501c8f8848afd00c6111158ef98cbaf6
tomasmansur <61087373+tomasmansur@users.noreply.github.com>
2022-10-01 22:17:30 -0300

    Update packages.sh

--- OpenWrt/packages.sh
+++ OpenWrt/packages.sh
@@ -1,4 +1,4 @@
-PACKAGES_BASIC_="stubby curl tcpdump bind-dig htop bmon"
-PACKAGES_EXTENDED_="wireguard-tools tor-basic wget-ssl wget-nossl nmap bash file"
+PACKAGES_BASIC_="stubby curl tcpdump bind-dig htop bmon dnsmasq-full -dnsmasq"
+PACKAGES_EXTENDED_="wireguard-tools tor-basic wget-ssl nmap bash file"
 PACKAGES_DEV_="gcc gdb python3 python3-pip"
 PACKAGES_RPI4_="kmod-usb-net-asix kmod-usb-net-rtl8152 kmod-ath9k-htc"
