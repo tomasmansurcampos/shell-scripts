fc805ff6b4b7e52ce16328fa501cf0bbabe05613
tomasmansur <61087373+tomasmansur@users.noreply.github.com>
2022-10-01 21:16:47 -0300

    Update youtubedl.sh

--- Debian/cli-software/youtubedl.sh
+++ Debian/cli-software/youtubedl.sh
@@ -2,7 +2,7 @@ youtube_dl_()
 {
 	## https://youtube-dl.org/
 	BASE_FOLDER_=$HOME
-	BINARY_FILE_=$BASE_FOLDER_/.local/share/youtube-dl
+	BINARY_FILE_=$BASE_FOLDER_/.local/youtube-dl
 	rm -rf $BINARY_FILE_
 	/usr/bin/wget --https-only https://yt-dl.org/downloads/latest/youtube-dl -qO $BINARY_FILE_
 	ln -sf $BINARY_FILE_ /usr/local/bin/youtube-dl
