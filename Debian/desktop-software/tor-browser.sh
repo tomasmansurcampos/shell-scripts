7de017f7e29ae6702e1f84873fe8c27ef7f8448c
tomasmansur <61087373+tomasmansur@users.noreply.github.com>
2022-10-01 21:18:11 -0300

    Update tor-browser.sh

--- Debian/desktop-software/tor-browser.sh
+++ Debian/desktop-software/tor-browser.sh
@@ -2,13 +2,13 @@
 tor_browser_()
 {
 	VERSION_=11.5.2
-	BASE_FOLDER_=/usr/local/share
+	BASE_FOLDER_=/usr/local
 	mkdir --parents $BASE_FOLDER_
 	wget --https-only https://www.torproject.org/dist/torbrowser/${VERSION_}/tor-browser-linux64-${VERSION_}_en-US.tar.xz
 	tar -xJf tor-browser-linux64-${VERSION_}_en-US.tar.xz
 	rm -rf tor-browser-linux64-${VERSION_}_en-US.tar.xz
 	mkdir --parents $BASE_FOLDER_
 	mv tor-browser_en-US $BASE_FOLDER_
-	chown -R user:user $BASE_FOLDER_/tor-browser_en-US
+	chown -R $1:$1 $BASE_FOLDER_/tor-browser_en-US
 }
 tor_browser_
