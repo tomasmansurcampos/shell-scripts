028a59814a0acb840323f91a2104137d503620d1
tomasmansur <61087373+tomasmansur@users.noreply.github.com>
2022-10-01 22:12:13 -0300

    Update dnsmasq.sh

--- OpenWrt/config-system/dnsmasq.sh
+++ OpenWrt/config-system/dnsmasq.sh
@@ -12,7 +12,7 @@ uci set dhcp.@dnsmasq[0].domain=""
 uci set dhcp.@dnsmasq[0].noresolv="1"
 uci set dhcp.@dnsmasq[0].localuse="1"
 uci set dhcp.@dnsmasq[0].cachesize="0"
-uci set dhcp.@dnsmasq[0].proxydnssec=1
+uci set dhcp.@dnsmasq[-1].proxydnssec=1
 uci -q delete dhcp.@dnsmasq[0].server
 uci -q delete dhcp.@dnsmasq[0].doh_backup_server
 uci add_list dhcp.@dnsmasq[0].server="127.0.0.1#5453"
