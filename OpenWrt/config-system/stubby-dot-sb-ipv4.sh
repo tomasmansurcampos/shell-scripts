05a3f1f011a0b9bbff29cbb63f90561640c4b335
tomasmansur <61087373+tomasmansur@users.noreply.github.com>
2022-10-02 11:46:40 -0300

    Update and rename stubby-cloudflare-ipv4.sh to stubby-dot-sb-ipv4.sh

--- OpenWrt/config-system/stubby-dot-sb-ipv4.sh
+++ OpenWrt/config-system/stubby-dot-sb-ipv4.sh
@@ -4,7 +4,7 @@ stubby_()
 {
   ## https://openwrt.org/docs/guide-user/services/dns/dot_dnsmasq_stubby
   ## https://github.com/openwrt/packages/blob/master/net/stubby/files/README.md
-  
+
   /etc/init.d/stubby stop
 
   uci set stubby.global.dns_transport='GETDNS_TRANSPORT_TLS'
@@ -13,17 +13,23 @@ stubby_()
   uci set stubby.global.tls_connection_retries="0"
   uci set stubby.global.tls_min_version="1.3"
   uci set stubby.global.tls_max_version="1.3"
-  uci set stubby.global.idle_timeout='9000' ## (NOTE: recommend reducing idle_timeout to 9000 if using Cloudflare)
+  uci set stubby.global.idle_timeout='10000'
   uci set stubby.global.appdata_dir="/tmp/stubby"
   uci set stubby.global.dnssec_return_status="1"
 
   while uci -q delete stubby.@resolver[0]; do :; done
+  ## https://dns.sb/dot/
   uci add stubby resolver
-  uci set stubby.@resolver[-1].address="1.1.1.1"
-  uci set stubby.@resolver[-1].tls_auth_name="1dot1dot1dot1.cloudflare-dns.com"
+  uci set stubby.@resolver[-1].address="45.11.45.11"
+  uci set stubby.@resolver[-1].tls_auth_name="dot.sb"
+  uci set stubby.@resolver[-1].tls_port="853"
+  uci add_list stubby.@resolver[-1].spki="sha256/0Ot+uUBCfWZkE2GFQQcIpR9GmuhWioGEl+K11FhNmHk="
+  ## https://dns.sb/dot/
   uci add stubby resolver
-  uci set stubby.@resolver[-1].address="1.0.0.1"
-  uci set stubby.@resolver[-1].tls_auth_name="1dot1dot1dot1.cloudflare-dns.com"
+  uci set stubby.@resolver[-1].address="185.222.222.222"
+  uci set stubby.@resolver[-1].tls_auth_name="dot.sb"
+  uci set stubby.@resolver[-1].tls_port="853"
+  uci add_list stubby.@resolver[-1].spki="sha256/0Ot+uUBCfWZkE2GFQQcIpR9GmuhWioGEl+K11FhNmHk="
 
   uci commit stubby
   /etc/init.d/stubby start
