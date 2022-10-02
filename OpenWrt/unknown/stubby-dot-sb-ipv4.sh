04b9d4816e4bcb44108d77164a5f04856a7b6f95
tomasmansur <61087373+tomasmansur@users.noreply.github.com>
2022-10-01 22:02:53 -0300

    Update stubby-dot-sb-ipv4.sh

--- OpenWrt/unknown/stubby-dot-sb-ipv4.sh
+++ OpenWrt/unknown/stubby-dot-sb-ipv4.sh
@@ -7,10 +7,15 @@ stubby_()
 
   /etc/init.d/stubby stop
 
+  uci set stubby.global.dns_transport='GETDNS_TRANSPORT_TLS'
+  uci set stubby.global.tls_authentication='1'
   uci set stubby.global.round_robin_upstreams="0"
-  uci set stubby.global.tls_connection_retries="1"
+  uci set stubby.global.tls_connection_retries="0"
   uci set stubby.global.tls_min_version="1.3"
   uci set stubby.global.tls_max_version="1.3"
+  uci set stubby.global.idle_timeout='10000'
+  uci set stubby.global.appdata_dir="/tmp/stubby"
+  uci set stubby.global.dnssec_return_status="1"
 
   while uci -q delete stubby.@resolver[0]; do :; done
   ## https://dns.sb/dot/
