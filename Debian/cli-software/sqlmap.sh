0bf85e2ddc4e111850b65232401ddfff2cee04bb
tomasmansur <61087373+tomasmansur@users.noreply.github.com>
2022-10-01 21:37:29 -0300

    Update sqlmap.sh

--- Debian/cli-software/sqlmap.sh
+++ Debian/cli-software/sqlmap.sh
@@ -6,6 +6,6 @@ sqlmap_()
 	apt -y install git
 	git clone --depth 1 https://github.com/sqlmapproject/sqlmap.git $SQLMAP_FOLDER_
 	ln -sf $BASE_FOLDER_/sqlmap-dev/sqlmap.py /usr/local/bin/sqlmap
-	echo "alias sqlmap='sqlmap -o --beep --force-ssl --mobile --smart'" >> /etc/profile.d/command_alias.sh
+	echo "alias sqlmap='sqlmap -o --beep --force-ssl --mobile --smart'" >> /etc/profile.d/user/command_alias.sh
 }
 sqlmap_
