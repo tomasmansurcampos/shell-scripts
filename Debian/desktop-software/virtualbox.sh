31e3797fd289f12e8c245ab27a8811c7d3add665
tomasmansur <61087373+tomasmansur@users.noreply.github.com>
2022-10-06 02:25:32 -0300

    Update virtualbox.sh

--- Debian/desktop-software/virtualbox.sh
+++ Debian/desktop-software/virtualbox.sh
@@ -5,7 +5,6 @@ virtual_box_()
 	SIGNING_KEY_FILE_=/etc/apt/trusted.gpg.d/virtual_box.gpg
 	wget --https-only -qO- https://www.virtualbox.org/download/oracle_vbox_2016.asc | gpg --dearmor | tee $SIGNING_KEY_FILE_ >/dev/null
 	echo -e "deb\t[arch=amd64 signed-by=${SIGNING_KEY_FILE_}] http://download.virtualbox.org/virtualbox/debian $(lsb_release -sc) contrib" > $SOURCE_LIST_FILE_
-	apt update
-	apt install -y virtualbox-6.1
+	#apt update && apt install -y virtualbox-6.1
 }
 virtual_box_
