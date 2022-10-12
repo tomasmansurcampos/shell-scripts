f6a9b00912f8dc0f64c2e5a8e66ae00a3ee0fc75
tomasmansur <61087373+tomasmansur@users.noreply.github.com>
2022-10-11 23:37:25 -0300

    Update google-chrome.sh

--- Debian/desktop-software/google-chrome.sh
+++ Debian/desktop-software/google-chrome.sh
@@ -6,6 +6,31 @@ google_chrome_()
 	apt update
 	apt install -y ./google-chrome-stable_current_amd64.deb
 	rm -rf google-chrome-stable_current_amd64.deb
+	
+	echo "[Desktop Entry]
+Version=1.0
+Name=Google Chrome (Private)
+# Only KDE 4 seems to use GenericName, so we reuse the KDE strings.
+# From Ubuntu's language-pack-kde-XX-base packages, version 9.04-20090413.
+GenericName=Web Browser
+# Gnome and KDE 3 uses Comment.
+Comment=Access the Internet
+Exec=/usr/bin/google-chrome-stable --incognito %U
+StartupNotify=true
+Terminal=false
+Icon=google-chrome
+Type=Application
+Categories=Network;WebBrowser;
+MimeType=application/pdf;application/rdf+xml;application/rss+xml;application/xhtml+xml;application/xhtml_xml;application/xml;image/gif;image/jpeg;image/png;image/webp;text/html;text/xml;x-scheme-handler/http;x-scheme-handler/https;
+Actions=new-window;new-private-window;
+
+[Desktop Action new-window]
+Name=New Window
+Exec=/usr/bin/google-chrome-stable --incognito
+
+[Desktop Action new-private-window]
+Name=New Incognito Window
+Exec=/usr/bin/google-chrome-stable --incognito" > /usr/share/applications/google-chrome-private.desktop
 }
 google_chrome_
 
