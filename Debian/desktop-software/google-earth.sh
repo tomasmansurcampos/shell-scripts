3df5551b725c6f0aead4a5735125acc6c4d12c9a
tomasmansur <61087373+tomasmansur@users.noreply.github.com>
2022-10-15 00:37:20 -0300

    Delete google-earth.sh

--- Debian/desktop-software/google-earth.sh
+++
@@ -1,7 +0,0 @@
-google_earth_()
-{
-	wget --https-only https://dl.google.com/dl/earth/client/current/google-earth-pro-stable_current_amd64.deb
-	apt install -y ./google-earth-pro-stable_current_amd64.deb
-	rm google-earth-pro-stable_current_amd64.deb
-}
-google_earth_
