bc0e158eaa3bdd17024e86a5752a74d330e67d48
tomasmansur <61087373+tomasmansur@users.noreply.github.com>
2022-12-17 14:00:11 -0300

    Update golang.sh

--- Debian/cli-software/golang.sh
+++ Debian/cli-software/golang.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-golang_bin_()
+
+install_()
 {
-    VERSION_=$(curl -s https://go.dev/dl/ | grep toggleVisible | head -n 1 | grep -Po 'id="\K[^"]+')
-    VERSION_=${VERSION_:2}
     URL_=https://go.dev/dl/go$VERSION_.linux-$(dpkg --print-architecture).tar.gz
     if wget --spider $URL_ 2>/dev/null; then
         wget --https-only $URL_
@@ -13,6 +12,36 @@ export PATH=$PATH:/usr/local/go/bin
 EOF
         rm -rf go$VERSION_.linux-$(dpkg --print-architecture).tar.gz
         source /root/.bashrc
+    fi
+}
+
+are_updated_()
+{
+    INSTALLED_VERSION_=$(go version | cut -d " " -f 3)
+    INSTALLED_VERSION_=${INSTALLED_VERSION_:2}
+    if [ "$(printf '%s\n' "$VERSION_" "$INSTALLED_VERSION_" | sort -V | head -n1)" = "$VERSION_" ]; then 
+        return 1
+    else
+        return 0
+    fi
+}
+
+golang_bin_()
+{
+    VERSION_=$(curl -s https://go.dev/dl/ | grep toggleVisible | head -n 1 | grep -Po 'id="\K[^"]+')
+    VERSION_=${VERSION_:2}
+
+    if ! command -v /usr/local/go/bin/go &> /dev/null
+    then
+        install_
+    else
+        if are_updated_
+        then
+            echo "Latest go version is installed!"
+        else
+            echo "Go language is not updated..."
+            echo "Installing latest version"
+            install_
     fi
 }
 golang_bin_
