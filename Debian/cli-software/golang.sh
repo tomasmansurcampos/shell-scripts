4f29b22021c0f0b5c09f509eb4433c24ecefc9d2
tomasmansur <61087373+tomasmansur@users.noreply.github.com>
2022-12-17 13:44:06 -0300

    Update golang.sh

--- Debian/cli-software/golang.sh
+++ Debian/cli-software/golang.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 golang_bin_()
 {
-    VERSION_=$(curl -s https://go.dev/dl/ | grep toggleVisible | head -n 1)
+    VERSION_=$(curl -s https://go.dev/dl/ | grep toggleVisible | head -n 1 | grep -Po 'id="\K[^"]+')
+    VERSION_=${VERSION_:2}
     URL_=https://go.dev/dl/go$VERSION_.linux-$(dpkg --print-architecture).tar.gz
     if wget --spider $URL_ 2>/dev/null; then
         wget --https-only $URL_
