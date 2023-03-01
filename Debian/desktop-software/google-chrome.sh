fc0affbbbb62997157d49add94eb5bc8dbc978c9
tomasmansur <61087373+tomasmansur@users.noreply.github.com>
2023-02-28 22:52:05 -0300

    Update google-chrome.sh

--- Debian/desktop-software/google-chrome.sh
+++ Debian/desktop-software/google-chrome.sh
@@ -3,6 +3,7 @@ google_chrome_()
 	## https://linuxnightly.com/how-to-install-google-chrome-on-debian-linux/
 	LINK_="https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"
 	wget --https-only $LINK_
+	rm -rf /etc/default/google-chrome
 	apt update
 	apt install -y ./google-chrome-stable_current_amd64.deb
 	rm -rf google-chrome-stable_current_amd64.deb
