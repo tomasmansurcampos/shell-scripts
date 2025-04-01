05e607ce58ae7701ff10ee21091e76df4e200c80
tomasmansur <61087373+tomasmansur@users.noreply.github.com>
2025-04-01 02:21:44 -0300

    Update blocking-shit.sh

--- blocking-shit.sh
+++ blocking-shit.sh
@@ -1,16 +1,17 @@
-### BLOCKING ADS TRACKERS MALWARE SPYWARE TELEMETRY CRYPTOMINING BY HOSTS FILE DIRECTLY
-cp /etc/hosts /etc/hosts.original
-echo "#!/bin/bash
+#!/bin/bash
 
 chattr -i /etc/hosts
 
 wget --inet4-only --https-only https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/fakenews-gambling/hosts -O /etc/hosts-steven-black
 wget --inet4-only --https-only https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/porn-only/hosts -O /etc/hosts-porn
 wget --inet4-only --https-only https://someonewhocares.org/hosts/zero/hosts -O /etc/hosts-dan-pollock
-wget --inet4-only --https-only https://raw.githubusercontent.com/badmojr/1Hosts/refs/heads/master/Lite/hosts.txt -O /etc/hosts-badmojr
-wget --inet4-only --https-only https://github.com/hagezi/dns-blocklists/raw/refs/heads/main/hosts/ultimate.txt -O /etc/hosts-hagezi-ultimate
+wget --inet4-only --https-only https://raw.githubusercontent.com/badmojr/1Hosts/refs/heads/master/Lite/hosts.txt -O /etc/hosts-1hosts-lite
+wget --inet4-only --https-only https://raw.githubusercontent.com/badmojr/1Hosts/refs/heads/master/Pro/hosts.txt -O /etc/hosts-1hosts-pro
+wget --inet4-only --https-only https://raw.githubusercontent.com/badmojr/1Hosts/refs/heads/master/Xtra/hosts.txt -O /etc/hosts-1hosts-xtra
+wget --inet4-only --https-only https://raw.githubusercontent.com/hagezi/dns-blocklists/main/hosts/multi.txt -O /etc/hosts-hagezi-multi
+wget --inet4-only --https-only https://raw.githubusercontent.com/hagezi/dns-blocklists/main/hosts/ultimate.txt -O /etc/hosts-hagezi-ultimate
+wget --inet4-only --https-only https://raw.githubusercontent.com/hagezi/dns-blocklists/main/hosts/tif.txt /etc/hosts-hagezi-tif
 wget --inet4-only --https-only https://big.oisd.nl/domainswild2 -O /etc/hosts-oisd
-wget --inet4-only --https-only https://github.com/badmojr/1Hosts/raw/refs/heads/master/Xtra/hosts.txt -O /etc/hosts-1hosts-xtra
 wget --inet4-only --https-only https://www.github.developerdan.com/hosts/lists/ads-and-tracking-extended.txt -O /etc/hosts-ads-and-tracking-extended
 wget --inet4-only --https-only https://www.github.developerdan.com/hosts/lists/tracking-aggressive-extended.txt -O /etc/hosts-tracking-aggressive-extended
 
@@ -24,20 +25,22 @@ cat /etc/hosts.original > /etc/hosts-ad-blocker
 cat /etc/hosts-steven-black >> /etc/hosts-ad-blocker
 cat /etc/hosts-porn >> /etc/hosts-ad-blocker
 cat /etc/hosts-dan-pollock >> /etc/hosts-ad-blocker
-cat /etc/hosts-badmojr >> /etc/hosts-ad-blocker
-cat /etc/hosts-hagezi-ultimate >> /etc/hosts-ad-blocker
-cat /etc/hosts-oisd-big >> /etc/hosts-ad-blocker
+cat /etc/hosts-1hosts-lite >> /etc/hosts-ad-blocker
+cat /etc/hosts-1hosts-pro >> /etc/hosts-ad-blocker
 cat /etc/hosts-1hosts-xtra >> /etc/hosts-ad-blocker
+cat /etc/hosts-hagezi-multi >> /etc/hosts-ad-blocker
+cat /etc/hosts-hagezi-ultimate >> /etc/hosts-ad-blocker
+cat /etc/hosts-hagezi-tif >> /etc/hosts-ad-blocker
+cat /etc/hosts-oisd-big >> /etc/hosts-ad-blocker
 cat /etc/hosts-ads-and-tracking-extended >> /etc/hosts-ad-blocker
 cat /etc/hosts-tracking-aggressive-extended >> /etc/hosts-ad-blocker
 
+sed -i -e 's/crash.steampowered.com/0.0.0.0/g' /etc/hosts-ad-blocker
 sed -i -e 's/web.facebook.com/0.0.0.0/g' /etc/hosts-ad-blocker
 sed -i -e 's/click.discord.com/0.0.0.0/g' /etc/hosts-ad-blocker
-sed -i -e 's/crash.steampowered.com/0.0.0.0/g' /etc/hosts-ad-blocker
 
+echo "0.0.0.0 www.something.com" >> /etc/hosts-ad-blocker
 
 awk '!seen[$0]++' /etc/hosts-ad-blocker > /etc/hosts
 
-chattr +i /etc/hosts" > /usr/bin/make-hosts-block-ads
-chmod +x /usr/bin/make-hosts-block-ads
-bash /usr/bin/make-hosts-block-ads
+chattr +i /etc/hosts
