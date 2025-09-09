e1abb810d84b29c57b64c1a1b404bbfae8e4feba
Tomas Mansur Campos <61087373+tomasmansurcampos@users.noreply.github.com>
2025-09-09 14:36:49 -0300

    Update debian.sh

--- debian/debian.sh
+++ debian/debian.sh
@@ -151,7 +151,7 @@ _networking()
 	### DISABLING IPV6
  	### SYSCTL.CONF FILE
 	cp -v /etc/sysctl.conf /etc/sysctl.conf.bak
-	cat << "EOF" > /etc/sysctl.d/99-noipv6.conf
+	cat << "EOF" > /etc/sysctl.d/50-noipv6.conf
 net.ipv6.conf.all.disable_ipv6 = 1
 net.ipv6.conf.default.disable_ipv6 = 1
 net.ipv6.conf.lo.disable_ipv6 = 1
