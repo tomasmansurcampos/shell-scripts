cfa8251728169094b3334ca3d6c24797f3c9c0b7
Tomas Mansur Campos <61087373+tomasmansurcampos@users.noreply.github.com>
2026-09-09 22:06:44 -0300

    Update musl-cc.sh

--- musl-cc.sh
+++ musl-cc.sh
@@ -8,11 +8,9 @@ musl_cc_()
     mkdir -p "${BASE_FOLDER}"
     cd "${BASE_FOLDER}"
 
-    # Filtrar directamente las URLs que contienen 'cross' y terminan en '.tgz'
     local links
     links=$(curl -sL "${BASE_URL}/" | grep 'cross.*\.tgz$')
 
-    # La variable links ya contiene las URLs completas
     for target_url in ${links}; do
         if wget -q --spider "${target_url}"; then
             wget --https-only --inet4-only -c "${target_url}"
